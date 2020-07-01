781413121bb01281bfb7ae296b012a2216185d8e
Paweł-Ś <pswiatlon02@gmail.com>
2020-07-01 10:09:22 +0200

    Add routerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
@@ -9,19 +10,31 @@ import { WeatherService } from './weather.service';
 import { CityWeatherComponent } from './city-weather/city-weather.component';
 import { ForecastWeatherComponent } from './forecast-weather/forecast-weather.component';
 import { MenuComponent } from './menu/menu.component';
-
+import { StatisticWeatherComponent } from './statistic-weather/statistic-weather.component';
+import { InformationComponent } from './information/information.component';
+import { ContactComponent } from './contact/contact.component';
+const appRout = [
+  {path: '', component: StatisticWeatherComponent},
+  {path: 'information', component: InformationComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '*', component: StatisticWeatherComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     CurrentWeatherComponent,
     CityWeatherComponent,
     ForecastWeatherComponent,
-    MenuComponent
+    MenuComponent,
+    StatisticWeatherComponent,
+    InformationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRout)
   ],
   providers: [WeatherService],
   bootstrap: [AppComponent]
---
+++ src/app/statistic-weather/statistic-weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistic-weather',
+  templateUrl: './statistic-weather.component.html',
+  styleUrls: ['./statistic-weather.component.css']
+})
+export class StatisticWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
